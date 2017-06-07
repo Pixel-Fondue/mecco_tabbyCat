5a0e0d1a4c5f63d78e70124566356ea26c1e62a0
adamohern <adam@mechanicalcolor.com>
2017-06-07 14:42:07 -0400

    error handling

--- lxserv/tabbyCat_tab.py
+++ lxserv/tabbyCat_tab.py
@@ -8,10 +8,10 @@ def selMode(target=None):
   modes = 'vertex;edge;polygon;item;pivot;center;ptag'
   if not target:
     for mode in modes.split(';'):
-        if lx.eval('select.typeFrom %s;%s ?' % (mode, modes)):
+        if lx.eval('!!select.typeFrom %s;%s ?' % (mode, modes)):
             return mode
   else:
-    lx.eval('select.typeFrom %s;%s' % (target, modes))
+    lx.eval('!!select.typeFrom %s;%s' % (target, modes))
 
 
 class CMD_tabbyCat_tabKey(lxu.command.BasicCommand):
@@ -24,96 +24,98 @@ class CMD_tabbyCat_tabKey(lxu.command.BasicCommand):
         return lx.symbol.fCMD_MODEL | lx.symbol.fCMD_UNDO
 
     def basic_Execute(self, msg, flags):
-        originalMode = selMode()
+        try:
+            originalMode = selMode()
 
-        selMode('polygon')
-        originalPolySel = lx.eval('query layerservice polys ? selected')
-        lx.eval('select.editSet originalPolySel set')
-        selMode(originalMode)
+            selMode('polygon')
+            originalPolySel = lx.eval('!!query layerservice polys ? selected')
+            lx.eval('!!select.editSet originalPolySel set')
+            selMode(originalMode)
 
-        if originalMode == 'vertex':
-          originalSel = lx.eval('query layerservice verts ? selected')
-          lx.eval('select.editSet originalSel set')
+            if originalMode == 'vertex':
+              originalSel = lx.eval('!!query layerservice verts ? selected')
+              lx.eval('!!select.editSet originalSel set')
 
-          selMode('polygon')
-          originalPolySel = lx.eval('query layerservice polys ? selected')
-          lx.eval('select.editSet originalPolySel set')
+              selMode('polygon')
+              originalPolySel = lx.eval('!!query layerservice polys ? selected')
+              lx.eval('!!select.editSet originalPolySel set')
 
-          selMode(originalMode)
-          if originalSel:
-            lx.eval('select.connect')
-          else:
-            lx.eval('select.all')
-          lx.eval('select.convert polygon')
+              selMode(originalMode)
+              if originalSel:
+                lx.eval('!!select.connect')
+              else:
+                lx.eval('!!select.all')
+              lx.eval('!!select.convert polygon')
 
-        elif originalMode == 'edge':
-          originalSel = lx.eval('query layerservice edges ? selected')
-          lx.eval('select.editSet originalSel set')
+            elif originalMode == 'edge':
+              originalSel = lx.eval('!!query layerservice edges ? selected')
+              lx.eval('!!select.editSet originalSel set')
 
-          selMode('polygon')
-          originalPolySel = lx.eval('query layerservice polys ? selected')
-          lx.eval('select.editSet originalPolySel set')
+              selMode('polygon')
+              originalPolySel = lx.eval('!!query layerservice polys ? selected')
+              lx.eval('!!select.editSet originalPolySel set')
 
-          selMode(originalMode)
-          if originalSel:
-            lx.eval('select.connect')
-          else:
-            lx.eval('select.all')
-          lx.eval('select.convert polygon')
+              selMode(originalMode)
+              if originalSel:
+                lx.eval('!!select.connect')
+              else:
+                lx.eval('!!select.all')
+              lx.eval('!!select.convert polygon')
 
-        elif originalMode == 'polygon':
-          originalSel = lx.eval('query layerservice polys ? selected')
-          lx.eval('select.editSet originalSel set')
+            elif originalMode == 'polygon':
+              originalSel = lx.eval('!!query layerservice polys ? selected')
+              lx.eval('!!select.editSet originalSel set')
 
-          if originalSel:
-            lx.eval('select.connect')
-          else:
-            lx.eval('select.all')
+              if originalSel:
+                lx.eval('!!select.connect')
+              else:
+                lx.eval('!!select.all')
 
-        elif originalMode == 'ptag':
-          selMode('polygon')
-          originalPolySel = lx.eval('query layerservice polys ? selected')
-          lx.eval('select.editSet originalPolySel set')
+            elif originalMode == 'ptag':
+              selMode('polygon')
+              originalPolySel = lx.eval('!!query layerservice polys ? selected')
+              lx.eval('!!select.editSet originalPolySel set')
 
-          selMode('ptag')
-          lx.eval('select.convert polygon')
-          if lx.eval('query layerservice polys ? selected'):
-            lx.eval('select.connect')
-          else:
-            lx.eval('select.all')
+              selMode('ptag')
+              lx.eval('!!select.convert polygon')
+              if lx.eval('!!query layerservice polys ? selected'):
+                lx.eval('!!select.connect')
+              else:
+                lx.eval('!!select.all')
 
-        elif originalMode in ['item','pivot','center']:
-          selMode('polygon')
-          lx.eval('select.all')
+            elif originalMode in ['item','pivot','center']:
+              selMode('polygon')
+              lx.eval('!!select.all')
 
-        selMode('polygon')
-        lx.eval('select.editSet extendedSel set')
+            selMode('polygon')
+            lx.eval('!!select.editSet extendedSel set')
 
-        lx.eval('select.polygon remove type face 0')
-        if lx.eval('query layerservice polys ? selected'):
-          target = "face"
-        else:
-          target = "psubdiv"
+            lx.eval('!!select.polygon remove type face 0')
+            if lx.eval('!!query layerservice polys ? selected'):
+              target = "face"
+            else:
+              target = "psubdiv"
 
-        lx.eval('select.useSet extendedSel replace')
-        lx.eval('poly.convert %s face toggle:false' % target)
-        lx.eval('!poly.align')
-        lx.eval('select.drop polygon')
+            lx.eval('!!select.useSet extendedSel replace')
+            lx.eval('!!poly.convert %s face toggle:false' % target)
+            lx.eval('!!poly.align')
+            lx.eval('!!select.drop polygon')
 
+            if originalPolySel:
+              lx.eval('!!select.useSet originalPolySel replace')
+            lx.eval('!!select.deleteSet originalPolySel')
+            lx.eval('!!select.deleteSet extendedSel')
 
+            selMode(originalMode)
 
-        if originalPolySel:
-          lx.eval('select.useSet originalPolySel replace')
-        lx.eval('select.deleteSet originalPolySel')
-        lx.eval('select.deleteSet extendedSel')
+            if originalMode in ['vertex','edge','polygon']:
+              lx.eval('!!select.drop %s' % originalMode)
+              if originalSel:
+                lx.eval('!!select.useSet originalSel replace')
 
-        selMode(originalMode)
+              lx.eval('!!select.deleteSet originalSel')
 
-        if originalMode in ['vertex','edge','polygon']:
-          lx.eval('select.drop %s' % originalMode)
-          if originalSel:
-            lx.eval('select.useSet originalSel replace')
-
-          lx.eval('select.deleteSet originalSel')
+        except:
+            pass
 
 lx.bless(CMD_tabbyCat_tabKey, NAME_CMD_tabKey)
